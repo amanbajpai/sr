407976b4559131c87463fba34d7e6c8a6788cc82
Dmitriy Makeev <dmma@ciklum.com>
2014-09-26 18:31:08 +0300

    Add support language

--- Matrix/src/main/java/com/ros/smartrocket/fragment/SettingsFragment.java
+++ Matrix/src/main/java/com/ros/smartrocket/fragment/SettingsFragment.java
@@ -41,7 +41,7 @@ public class SettingsFragment extends Fragment implements CompoundButton.OnCheck
         AdapterView.OnItemSelectedListener {
     private PreferencesManager preferencesManager = PreferencesManager.getInstance();
     public static final String DEFAULT_LANG = java.util.Locale.getDefault().toString();
-    public static final String[] SUPPORTED_LANGS_CODE = new String[]{"en", "zh_CN", "zh_TW"};
+    public static final String[] SUPPORTED_LANGS_CODE = new String[]{"en", "zh", "zh_CN", "zh_TW", "en_SG", "zh_HK"};
     public static String[] SUPPORTED_LANGUAGE = new String[]{"English", "中文（简体）",
             "中文（繁體）"};
     public static final int[] MONTHLY_LIMIT_MB_CODE = new int[]{0, 50, 100, 250, 500};
