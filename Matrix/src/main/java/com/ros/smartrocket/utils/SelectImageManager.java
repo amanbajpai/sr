a2b65ef5f9f1e6c952488777d2705fd383208511
Anton Derevyanko <anton.derevyanko@gmail.com>
2015-06-18 13:56:51 +0300

    Back-up Gallery changes

--- Matrix/src/main/java/com/ros/smartrocket/utils/SelectImageManager.java
+++ Matrix/src/main/java/com/ros/smartrocket/utils/SelectImageManager.java
@@ -74,7 +74,7 @@ public class SelectImageManager {
     public void startGallery(Activity activity) {
         this.activity = activity;
 
-        Intent intent = new Intent(Intent.ACTION_PICK);
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         if (!IntentUtils.isIntentAvailable(activity, intent)) {
             intent = new Intent(Intent.ACTION_GET_CONTENT);
             intent.setType("image/*");
