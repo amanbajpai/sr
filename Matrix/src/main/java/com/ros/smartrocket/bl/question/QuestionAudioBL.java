31853dc2d3c9d22ac1902b35f5e5bfac3c07bc3a
Петр <ppo@ciklum.com>
2017-07-06 17:39:24 +0300

    UI was implemented

--- Matrix/src/main/java/com/ros/smartrocket/bl/question/QuestionAudioBL.java
+++ Matrix/src/main/java/com/ros/smartrocket/bl/question/QuestionAudioBL.java
@@ -1,16 +1,102 @@
 package com.ros.smartrocket.bl.question;
 
+import android.support.v7.widget.AppCompatImageView;
+import android.view.View;
+
+import com.ros.smartrocket.R;
 import com.ros.smartrocket.db.entity.Answer;
+import com.ros.smartrocket.helpers.AVDWrapper;
+import com.shuyu.waveview.AudioWaveView;
 
-public class QuestionAudioBL extends QuestionBaseBL {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
+public class QuestionAudioBL extends QuestionBaseBL implements AVDWrapper.AnimationCallback {
+    private static final int ANIMATION_DURATION = 470;
+    @Bind(R.id.btnRecord)
+    AppCompatImageView btnRecord;
+    @Bind(R.id.btnPause)
+    AppCompatImageView btnPause;
+    @Bind(R.id.btnStop)
+    AppCompatImageView btnStop;
+    @Bind(R.id.btnPlay)
+    AppCompatImageView btnPlay;
+    @Bind(R.id.btnTrash)
+    AppCompatImageView btnTrash;
+    @Bind(R.id.audioWave)
+    AudioWaveView audioWave;
+    private AVDWrapper animationWrapper;
 
     @Override
     public void configureView() {
+        ButterKnife.bind(view);
+        animationWrapper = new AVDWrapper();
+        animationWrapper.setCallback(this);
     }
-
 
     @Override
     public void fillViewWithAnswers(Answer[] answers) {
+    }
+
+    private void handleDeleteClick() {
+        btnRecord.setVisibility(View.VISIBLE);
+        btnTrash.setVisibility(View.GONE);
+        btnPlay.setVisibility(View.GONE);
+        btnPause.setVisibility(View.GONE);
+    }
+
+    private void handlePlayClick() {
+        btnPause.setVisibility(View.VISIBLE);
+        btnPlay.setVisibility(View.GONE);
+    }
+
+    private void handleStopClick() {
+        btnPlay.setVisibility(View.VISIBLE);
+        btnTrash.setVisibility(View.VISIBLE);
+        btnPause.setVisibility(View.GONE);
+        btnRecord.setVisibility(View.GONE);
+        btnStop.setVisibility(View.GONE);
+    }
+
+    private void handlePauseClick() {
+        btnPause.setVisibility(View.GONE);
+        btnRecord.setVisibility(View.VISIBLE);
+        btnStop.setVisibility(View.VISIBLE);
+    }
+
+    private void handleRecordClick() {
+        btnRecord.setVisibility(View.GONE);
+        btnPause.setVisibility(View.VISIBLE);
+        btnStop.setVisibility(View.VISIBLE);
+    }
+
+
+    @OnClick({R.id.btnRecord, R.id.btnPause, R.id.btnStop, R.id.btnPlay, R.id.btnTrash, R.id.audioWave})
+    public void onViewClicked(View view) {
+        if (animationWrapper != null && view instanceof AppCompatImageView) {
+            animationWrapper.start(ANIMATION_DURATION, (AppCompatImageView) view);
+        }
+    }
+
+    @Override
+    public void onAnimationDone(AppCompatImageView imageView) {
+        switch (imageView.getId()) {
+            case R.id.btnRecord:
+                handleRecordClick();
+                break;
+            case R.id.btnPause:
+                handlePauseClick();
+                break;
+            case R.id.btnStop:
+                handleStopClick();
+                break;
+            case R.id.btnPlay:
+                handlePlayClick();
+                break;
+            case R.id.btnTrash:
+                handleDeleteClick();
+                break;
+        }
     }
 }
--- Matrix/src/main/java/com/ros/smartrocket/helpers/AVDWrapper.java
+++ Matrix/src/main/java/com/ros/smartrocket/helpers/AVDWrapper.java
@@ -2,36 +2,38 @@ package com.ros.smartrocket.helpers;
 
 import android.graphics.drawable.Animatable;
 import android.os.Handler;
+import android.support.v7.widget.AppCompatImageView;
 
 
 public class AVDWrapper {
 
     private Handler handler;
-    private Animatable animatable;
-    private Callback callback;
+    private AppCompatImageView imageView;
+    private AnimationCallback callback;
     private Runnable mAnimationDoneRunnable = new Runnable() {
         @Override
         public void run() {
-            if (callback != null)
-                callback.onAnimationDone();
+            if (callback != null) {
+                callback.onAnimationDone(imageView);
+            }
         }
     };
 
-    public AVDWrapper(Animatable animatable) {
-        this.animatable = animatable;
+    public AVDWrapper() {
         handler = new Handler();
     }
 
-    public void start(long duration) {
-        animatable.start();
+    public void start(long duration, AppCompatImageView imageView) {
+        this.imageView = imageView;
+        ((Animatable) imageView.getDrawable()).start();
         handler.postDelayed(mAnimationDoneRunnable, duration);
     }
 
-    public void setCallback(Callback callback) {
+    public void setCallback(AnimationCallback callback) {
         this.callback = callback;
     }
 
-    public interface Callback {
-        void onAnimationDone();
+    public interface AnimationCallback {
+        void onAnimationDone(AppCompatImageView imageView);
     }
 }
