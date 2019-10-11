082995f49867a3d575dd89a3c48b78fc0c806455
devendrasahu <devendrasahu@cdnsol.com>
2019-10-11 15:14:02 +0530

    SR 130 fixes

--- Matrix/src/main/java/com/ros/smartrocket/WorkManager/WorkManagerScheduler.java
+++ Matrix/src/main/java/com/ros/smartrocket/WorkManager/WorkManagerScheduler.java
@@ -1,12 +1,13 @@
 package com.ros.smartrocket.WorkManager;
 
-import com.ros.smartrocket.Keys;
-
 import androidx.work.Constraints;
 import androidx.work.Data;
+import androidx.work.ExistingWorkPolicy;
 import androidx.work.NetworkType;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkManager;
+
+import com.ros.smartrocket.Keys;
 
 public class WorkManagerScheduler {
 
@@ -16,33 +17,35 @@ public class WorkManagerScheduler {
     public static void callWorkManager(String actionCheckNotUploadedFiles) {
 
         Constraints constraints = new Constraints.Builder()
-                .setRequiresCharging(true)
                 .setRequiredNetworkType(NetworkType.CONNECTED)
                 .build();
 
         Data data = new Data.Builder()
                 .putString(Keys.GET_KEY_FROM_WORKER, actionCheckNotUploadedFiles)
                 .build();
-//
+
         if (actionCheckNotUploadedFiles.equals(Keys.ACTION_START_REMINDER_TIMER)) {
 
             mWorkRequest = new OneTimeWorkRequest.Builder(TaskRemainderServiceWorker.class)
                     .setInputData(data)
                     .setConstraints(constraints)
                     .build();
+            WorkManager.getInstance().enqueueUniqueWork("task_reminder_worker", ExistingWorkPolicy.KEEP, mWorkRequest);
+
         } else if (actionCheckNotUploadedFiles.equals(Keys.ACTION_STOP_REMINDER_TIMER)) {
 
             mWorkRequest = new OneTimeWorkRequest.Builder(TaskRemainderServiceWorker.class)
                     .setInputData(data)
                     .setConstraints(constraints)
                     .build();
+            WorkManager.getInstance().enqueueUniqueWork("task_reminder_worker", ExistingWorkPolicy.KEEP, mWorkRequest);
         } else {
             mWorkRequest = new OneTimeWorkRequest.Builder(UploadFileServiceWorker.class)
                     .setInputData(data)
                     .setConstraints(constraints)
                     .build();
-
+            WorkManager.getInstance().enqueueUniqueWork("upload_file_worker", ExistingWorkPolicy.KEEP, mWorkRequest);
         }
-        WorkManager.getInstance().enqueue(mWorkRequest);
+
     }
 }
